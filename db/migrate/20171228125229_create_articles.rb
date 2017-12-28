5f423be8a428d7937e249a0def54609049755655
Kaioru <keifuurawrs@gmail.com>
2017-12-28 21:02:36 +0800

    Add controllers

--- db/migrate/20171228125229_create_articles.rb
+++ db/migrate/20171228125229_create_articles.rb
@@ -2,6 +2,9 @@ class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
       t.string :url
+      t.string :image_url
+      t.string :title
+      t.string :description
     end
   end
 end
