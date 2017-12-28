b83f77353499bb1283f83693bb44c52315aff2b8
Kaioru <keifuurawrs@gmail.com>
2017-12-28 22:38:19 +0800

    Woops

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -11,7 +11,7 @@ class StatsController < ApplicationController
     render json: {
         "url" => @url,
         "confidence" => @stats.round(2),
-        "is_fake" => @stats > 7
+        "is_fake" => @stats > 0.7
       }
   end
 
